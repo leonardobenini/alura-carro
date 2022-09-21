bf6d098680d2bd105ba182e406939e311b9ac9d1
Leonardo Benini <103040881+leonardobenini@users.noreply.github.com>
2022-09-20 21:01:31 -0300

    Update ator.js

---
+++ ator.js
@@ -0,0 +1,62 @@
+//ator
+
+let yAtor = 370;
+let xAtor = 85;
+let colisao = false;
+let meusPontos = 0;
+
+function mostraAtor(){
+  image(imagemDoAtor, xAtor, yAtor, 25, 25);
+}
+
+function movimentaAtor(){
+  if (keyIsDown(UP_ARROW)){
+    yAtor -= 3;
+  }
+  if (keyIsDown(DOWN_ARROW)){
+    if(podeSeMover()){
+    yAtor += 3;
+    }
+  }
+}
+
+function verificaColisao(){
+  //collideRectCircle(x1, y1, width1, height1, cx, cy, diameter)
+  for (let i = 0; i < imagemCarros.length; i++){
+    colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15)
+    if (colisao){
+      voltaAtorPosicaoInicial();
+      somDaColisao.play();
+      if (pontosMaiorQueZero()){
+      meusPontos -= 1;
+     }   
+    }
+  }
+}
+
+function voltaAtorPosicaoInicial(){
+  yAtor = 370;
+}
+
+function incluiPontos(){
+  textAlign(CENTER);
+  textSize(25);
+  fill(color(255,240, 60));
+  text(meusPontos, width / 5, 27);
+}
+
+function marcaPonto(){
+  if (yAtor < 15){
+    meusPontos += 1;
+    somDoPonto.play();
+    voltaAtorPosicaoInicial();
+  }
+}
+
+function pontosMaiorQueZero(){
+  return meusPontos > 0
+}
+
+function podeSeMover(){
+  return yAtor < 370;
+}
